2da0f7551711fadfc347e1b5f13dcc1a4fe9fdda
Emma Gerigscott <elgerig@gmail.com>
2022-04-12 15:35:42 -0700

    Add prompt for user input, only rock results thus far

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-function computerPlay(options) {
-    return (options)[Math.floor(Math.random()*(options).length)];
-}
-const options = ['Rock', 'Paper', 'Scissors']
 
-function playRound(playerSelection, computerSelection) {
-    if (computerSelection === 'Rock') {
-        return `Computer played ${computerSelection}. It's a tie!`
-    } else if (computerSelection === 'Paper'){
-        return `You lose! Computer played ${computerSelection}`
-    } else if (computerSelection === 'Scissors') {
-        return `You win! Computer played ${computerSelection}`
-    }
-}
+let userScore = 0;
+let compScore = 0;
 
-const playerSelection = "Rock";
-const computerSelection = computerPlay(options);
-console.log(playRound(playerSelection, computerSelection));
+function computerPlay() {
+    let number = Math.floor(Math.random() * (3+1));
+    if (number == 1)
+        return 'Rock';
+    else if(number == 2)
+        return 'Paper';
+    else return 'Scissors';
+}
 
 function game(playRound) {
     for (let i = 0; i < 5; i++) {
-        
      }
 }
+
+let computerSelection = computerPlay();
+let playerSelection = prompt("Rock, Paper, or Scissors?")
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection === 'Rock' && (playerSelection.toUpperCase() === 'ROCK')) {
+        return `Computer played ${computerSelection}. It's a tie!`
+    } else if (computerSelection === 'Paper' && (playerSelection.toUpperCase() === 'ROCK')) {
+        return `You lose! Computer played ${computerSelection}`
+    } else if (computerSelection === 'Scissors' && (playerSelection.toUpperCase() === 'ROCK')) {
+        return `You win! Computer played ${computerSelection}`
+    } 
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+
