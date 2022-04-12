b88d10ee4c90c028dd1bd39b1464be6842b07444
Emma Gerigscott <elgerig@gmail.com>
2022-04-12 13:10:44 -0700

    Attempt 1 of func playRound (unsuccessful)

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function computerPlay(rps) {
 
 const rps =['Rock', 'Paper', 'Scissors'];
 console.log(computerPlay(rps));
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a tie!`
+    } else if (playerSelection == "rock" && computerSelection == "Paper") {
+        return 'You Lose! Paper beats rock!' 
+    } else if (playerSelection == "paper" && computerSelection == "Rock") {
+        return `You win! Paper beats rock!`
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay(rps);
+console.log(playRound(playerSelection, computerSelection));
