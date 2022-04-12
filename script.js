8606149fcaa0daab95b8f8aed17c44fdf3e609de
Emma Gerigscott <elgerig@gmail.com>
2022-04-12 13:46:18 -0700

    It does something but not the right thing...

--- script.js
+++ script.js
@@ -6,17 +6,13 @@ const rps =['Rock', 'Paper', 'Scissors'];
 console.log(computerPlay(rps));
 
 function playRound(playerSelection, computerSelection) {
-    if ((computerSelection === 'Rock' && playerSelection === 'paper') OR 
-    (computerSelection === 'Paper' && playerSelection === 'Scissors') OR 
-    (computerSelection === 'Scissors' && playerSelection === 'rock')) {
-        return `Nice work! You win...this round`
-    } else if ((computerSelection === 'Paper' && playerSelection === 'rock') OR
-    (computerSelection === 'Rock' && playerSelection === 'Scissors') OR
-    (computerSelection === 'Scissors' && playerSelection === 'paper')) {
-        return `Too bad! You lose!`
+    if (playerSelection === computerSelection) {
+        return `I guess it's a tie.`
+    } else if (playerSelection !== computerSelection) {
+        return `who the fuck knows, man`
     }
 }
 
-const playerSelection = "rock";
+const playerSelection = "Rock";
 const computerSelection = computerPlay(rps);
 console.log(playRound(playerSelection, computerSelection));
