1f5dfe7c6023e87db0bfed63882877983bfd0c28
Emma Gerigscott <elgerig@gmail.com>
2022-04-13 08:13:06 -0700

    Add all other return options of RPS

--- script.js
+++ script.js
@@ -26,10 +26,18 @@ function playRound(playerSelection, computerSelection) {
         return `You lose! Computer played ${computerSelection}`
     } else if (computerSelection === 'Scissors' && (playerSelection.toUpperCase() === 'ROCK')) {
         return `You win! Computer played ${computerSelection}`
-    } 
+    } else if (computerSelection === 'Rock' && (playerSelection.toUpperCase() === 'PAPER')) {
+        return `You win! Computer played ${computerSelection}`
+    } else if (computerSelection === 'Paper' && (playerSelection.toUpperCase() === 'PAPER')) {
+        return `Computer played ${computerSelection}. It's a tie!`
+    } else if (computerSelection === 'Scissors' && (playerSelection.toUpperCase() === 'PAPER')) {
+        return `You lose! Computer played ${computerSelection}`
+    } else if (computerSelection === 'Rock' && (playerSelection.toUpperCase() === 'SCISSORS')) {
+        return `You LOSE! Computer played ${computerSelection}`
+    } else if (computerSelection === 'Paper' && (playerSelection.toUpperCase() === 'SCISSORS')) {
+        return `Computer played ${computerSelection}. YOU WIN!`
+    } else if (computerSelection === 'Scissors' && (playerSelection.toUpperCase() === 'SCISSORS')) {
+        return `It's a tie! Computer played ${computerSelection}`
+    }
 }
-
 console.log(playRound(playerSelection, computerSelection));
-
-
-
