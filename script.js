8ffae1d7dbee37036e370f1a617ad21498c15264
Emma Gerigscott <elgerig@gmail.com>
2022-04-18 14:33:22 -0700

    Simply playRound function, game score display

--- script.js
+++ script.js
@@ -1,65 +1,45 @@
-//Global variables
+const buttons = document.querySelectorAll('input');
+const result = document.querySelector('div#result');
+const score = document.querySelector('div.score');
+
 let userScore = 0;
 let compScore = 0;
 
-//computer selects random Rock Paper Scissors
 function computerPlay() {
-    let number = Math.floor(Math.random() * (3+1));
-    if (number == 1)
-        return 'Rock';
-    else if(number == 2)
-        return 'Paper';
-    else return 'Scissors';
+    let choices = [`Rock`, `Paper`, `Scissors`]
+    return choices[Math.floor(Math.random() * choices.length)]
 }
 
-
-//Plays one round of the game
-function playRound(playerSelection, computerSelection) {
-    if (computerSelection === 'Rock' && (playerSelection.toUpperCase() === 'ROCK')) {
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
+//for a tie
+    if (computerSelection === playerSelection) {
         return `Computer played ${computerSelection}. It's a tie!`
-    } else if (computerSelection === 'Paper' && (playerSelection.toUpperCase() === 'ROCK')) {
-        compScore++;
-        return `You lose! Computer played ${computerSelection}`
-    } else if (computerSelection === 'Scissors' && (playerSelection.toUpperCase() === 'ROCK')) {
-        userScore++;
-        return `You win! Computer played ${computerSelection}`
-    } else if (computerSelection === 'Rock' && (playerSelection.toUpperCase() === 'PAPER')) {
-        userScore++;
-        return `You win! Computer played ${computerSelection}`
-    } else if (computerSelection === 'Paper' && (playerSelection.toUpperCase() === 'PAPER')) {
-        return `Computer played ${computerSelection}. It's a tie!`
-    } else if (computerSelection === 'Scissors' && (playerSelection.toUpperCase() === 'PAPER')) {
-        compScore++;
-        return `You lose! Computer played ${computerSelection}`
-    } else if (computerSelection === 'Rock' && (playerSelection.toUpperCase() === 'SCISSORS')) {
-        compScore++;
-        return `You LOSE! Computer played ${computerSelection}`
-    } else if (computerSelection === 'Paper' && (playerSelection.toUpperCase() === 'SCISSORS')) {
+        //user wins
+    } else if ((computerSelection === 'Paper' && (playerSelection === `Scissors`)) || 
+            (computerSelection === 'Rock' && (playerSelection === `Paper`)) ||
+            (computerSelection === 'Scissors' && (playerSelection === `Rock`))) { 
         userScore++;
         return `Computer played ${computerSelection}. YOU WIN!`
-    } else if (computerSelection === 'Scissors' && (playerSelection.toUpperCase() === 'SCISSORS')) {
-        return `It's a tie! Computer played ${computerSelection}`
+
+        if (userScore == 5) {
+            score.textContent = `You won the game!`
+        }
+        //comp wins
+} else {
+    compScore++;
+    return `You lose! Computer played ${computerSelection}.`
+
+    if (compScore == 5) {
+        score.textContent = `Computer won the game! Reload the page to play again.`
     }
 }
-
-//This loops through 5 rounds of the playRound function.
-//
-function game() {
-    for (let i = 0; i < 5; i++) {
-//computerSelection and playerSelection can be defined in this function because 
-//it nests or whatever playRound, so playRound can refer to the const and let in this function to run.
-        const computerSelection = computerPlay();
-        let playerSelection = prompt("Rock, Paper, or Scissors?");
-        console.log(playRound(playerSelection, computerSelection))
-        console.log(`YOU ${userScore} - COMPUTER ${compScore}`)
-   }
-   if (userScore < compScore) {
-       console.log("Computer Wins! Better try again.")
-   } else if (userScore > compScore) {
-       console.log("YOU WON THE WHOLE GAME WOW!")
-   } else if (userScore == compScore) {
-       console.log("It's a DRAW!")
-   }
 }
 
-game(playRound)
+buttons.forEach(button =>{
+    button.addEventListener('click', () => {
+        result.textContent = `${playRound(button.value)}`
+        score.textContent = `You have ${userScore}, Computer has ${compScore}`;
+    })
+})
+
